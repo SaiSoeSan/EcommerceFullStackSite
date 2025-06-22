eefa40a7329f0870fdf41846c61d6e86a311d451
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2025-06-21 21:26:33 -0500

    backend read static folder

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import { MongoClient } from 'mongodb';
+import express from "express";
+import { MongoClient } from "mongodb";
 
 // Load environment variables from .env file
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); 
-
+const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT || 3000;
 const username = process.env.DB_USERNAME;
@@ -19,24 +18,27 @@ const dbName = process.env.DB_NAME;
 const appName = process.env.APP_NAME;
 const url = `mongodb+srv://${username}:${password}@${clusterUrl}/?retryWrites=true&w=majority&appName=${appName}`;
 
-
 async function startServer() {
   const client = new MongoClient(url);
   await client.connect();
   const db = client.db(dbName);
 
   const app = express();
-  app.use(express.json()); // Middleware to parse JSON bodies
+  // Middleware to parse JSON bodies
 
-  app.use('/images', express.static(path.join(__dirname, '../assets')));
+  // Serve static files from the Vue.js build folder
+  app.use(express.static(path.join(__dirname, "../dist")));
 
+  app.use("/images", express.static(path.join(__dirname, "../assets")));
 
-  async function populatedCartItems(ids){
-    return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
+  async function populatedCartItems(ids) {
+    return Promise.all(
+      ids.map((id) => db.collection("products").findOne({ id }))
+    );
   }
 
   async function getProductById(id) {
-    const product = await db.collection('products').findOne({ id });
+    const product = await db.collection("products").findOne({ id });
     if (!product) {
       throw new Error(`Product with id ${id} not found`);
     }
@@ -44,28 +46,28 @@ async function startServer() {
   }
 
   //Get cart items for a user
-  app.get('/api/users/:userId/cart', async (req,res) => {
-    const collection = db.collection('users');
+  app.get("/api/users/:userId/cart", async (req, res) => {
+    const collection = db.collection("users");
     const user = await collection.findOne({ id: req.params.userId });
     const populatedCart = await populatedCartItems(user?.cartItems || []);
     res.json(populatedCart);
   });
 
-  app.get('/api/products', async (req, res) => {
-    const collection = db.collection('products');
+  app.get("/api/products", async (req, res) => {
+    const collection = db.collection("products");
     const products = await collection.find({}).toArray();
     res.json(products);
   });
 
-  app.get('/api/products/:id', async (req, res) => {
-    console.log(req.headers['x-user-id']);
-    const userId = req.headers['x-user-id'];
+  app.get("/api/products/:id", async (req, res) => {
+    console.log(req.headers["x-user-id"]);
+    const userId = req.headers["x-user-id"];
     const product = await getProductById(req.params.id);
     // check if the product is in the cart
-    if(!userId) {
+    if (!userId) {
       product.isInCart = false;
-    }else{
-      const collection = db.collection('users');
+    } else {
+      const collection = db.collection("users");
       const user = await collection.findOne({ id: userId });
       if (!user) {
         product.isInCart = false;
@@ -78,10 +80,10 @@ async function startServer() {
   });
 
   //add a product to cart
-  app.post('/api/users/:userId/cart', async (req, res) => {
+  app.post("/api/users/:userId/cart", async (req, res) => {
     const userId = req.params.userId;
     const productId = req.body.id;
-    const collection = db.collection('users');
+    const collection = db.collection("users");
 
     // Check user is exists
     const existingUser = await collection.findOne({ id: userId });
@@ -89,9 +91,12 @@ async function startServer() {
       collection.insertOne({ id: userId, cartItems: [] });
     }
 
-    await collection.updateOne({id : userId},{
-      $addToSet:{cartItems: productId}
-    });
+    await collection.updateOne(
+      { id: userId },
+      {
+        $addToSet: { cartItems: productId },
+      }
+    );
 
     const user = await collection.findOne({ id: userId });
     const populatedCart = await populatedCartItems(user?.cartItems || []);
@@ -99,26 +104,26 @@ async function startServer() {
   });
 
   //delete a product from cart
-  app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
+  app.delete("/api/users/:userId/cart/:productId", async (req, res) => {
     const productId = req.params.productId;
     const userId = req.params.userId;
-    const collection = db.collection('users');
+    const collection = db.collection("users");
 
-    await collection.updateOne({id : userId},{
-      $pull:{cartItems: productId}    
-    })
+    await collection.updateOne(
+      { id: userId },
+      {
+        $pull: { cartItems: productId },
+      }
+    );
 
     const user = await collection.findOne({ id: userId });
     const populatedCart = await populatedCartItems(user?.cartItems || []);
     res.json(populatedCart);
-    
   });
 
   app.listen(port, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log("Server is running on http://localhost:" + port);
   });
 }
 
 startServer();
-
-
